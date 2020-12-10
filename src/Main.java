4d23e6e666b8bb74dd92716fa15b393a91a1fc4d
PhilippRiefer <66263398+PhilippRiefer@users.noreply.github.com>
2020-12-10 14:58:35 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*	Programm : Main . java
+*	Autoren : Philipp Riefer, Domenic Heidemann
+*	Datum : 10.12.2020
+*/
+
+public class Main {
+	public static void main(String[] args) {
+		Buffer trump = new Buffer();
+		trump.put("bitch");
+		trump.put("racist");
+		trump.put("liar");
+		trump.put("mysogonist");
+		System.out.println("trump = "+ trump.get());
+	}
+}
